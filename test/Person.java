f6c87c242f698e3441e8af002036669a9207b13a
Alexth <zelechos@hotmail.com>
2024-01-06 11:42:06 -0400

    feat: test java scripts

---
+++ test/Person.java
@@ -0,0 +1,37 @@
+public class Person {
+  
+  public String name;
+  public String lastname;
+  public int age;
+
+  public Person(String name, String lastname, int age){
+    this.name = name;
+    this.lastname = lastname;
+    this.age = age;
+  }
+
+  public String getName(){
+    return this.name;
+  }
+
+  public void setName(String name){
+    this.name = name;
+  }
+  
+  public String getLastName(){
+    return this.lastname;
+  }
+
+  public void setLastName(String lastname){
+    this.lastname = lastname;
+  }
+
+  public int getAge(){
+    return this.age;
+  }
+
+  public void setAge(int age){
+    this.age = age;
+  }
+
+}
--- test/main.java
+++ test/main.java
@@ -1,5 +1,18 @@
+package test;
+
+import java.util.*;
+
 public class Main {
+
   public static void main (String[] args){
-    System.out.println("Hello world desde vim");
+    System.out.println(factoryPersons());
+  }
+  
+  public static List<Person> factoryPersons(){
+    List<Person> persons = new ArrayList<>();
+    persons.add(new Person("Alex", "Tumiri Huanca", 27));
+    persons.add(new Person("Alan", "Turing", 43));
+    persons.add(new Person("Linus", "Torvalds", 53));
+    return persons;
   }
 }
