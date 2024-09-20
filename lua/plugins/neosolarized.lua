908496ba92af483dcd7793793995264fb2bc9442
Alex T.H. <zelechos@hotmail.com>
2024-09-20 14:03:01 -0400

    style(v0.1.7): add transparent nvim

--- lua/plugins/neosolarized.lua
+++ lua/plugins/neosolarized.lua
@@ -3,6 +3,11 @@ return {
   lazy = false,
   priority = 1000,
   config = function()
+    require("tokyonight").setup({
+      style = "storm", -- puedes usar 'storm', 'night', 'day', etc.
+      transparent = true, -- activa el fondo transparente
+      terminal_colors = true, -- si quieres que el fondo del terminal también sea transparente
+    })
     vim.cmd([[colorscheme tokyonight]])
   end,
 }
